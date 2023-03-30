d688659750263da8f441b7a1366839cf6676ed5b
Yevhen <eug.z2004@gmail.com>
2023-03-30 11:29:03 +0300

    added parametrizing test to sort_by_population function

---
+++ test_main.py
@@ -0,0 +1,19 @@
+import pytest
+from main import sort_by_population
+
+
+@pytest.mark.parametrize(
+    'data, descending_order, result',
+    [
+        ([['Ukraine', 602000, 38000000], ['Poland', 312696, 37500000], ['USA', 9834000, 331900000], ['Germany', 357588, 83200000], ['Japan', 377973, 125700000]],
+        True,
+        [['USA', 9834000, 331900000], ['Japan', 377973, 125700000], ['Germany', 357588, 83200000], ['Ukraine', 602000, 38000000], ['Poland', 312696, 37500000]]
+         ),
+([['Ukraine', 602000, 38000000], ['Poland', 312696, 37500000], ['USA', 9834000, 331900000], ['Germany', 357588, 83200000], ['Japan', 377973, 125700000]],
+        False,
+        [['Poland', 312696, 37500000], ['Ukraine', 602000, 38000000], ['Germany', 357588, 83200000], ['Japan', 377973, 125700000], ['USA', 9834000, 331900000]]
+         )
+    ]
+)
+def test_sort_by_population(data, descending_order, result):
+    assert sort_by_population(data, descending_order) == result
