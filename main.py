e342de999f44ac54ca3125004730ad26fe27cbff
Yevhen <eug.z2004@gmail.com>
2023-03-30 11:12:45 +0300

    added pretty_print_the_data method

--- main.py
+++ main.py
@@ -17,11 +17,21 @@ def sort_by_area(data, descending_order=False):
     return sorted(data, key=lambda x: x[1], reverse=descending_order)
 
 
+def pretty_print_the_data(data):
+    counter = 1
+    for item in data:
+        print(f'\nCountry {counter}\nName: {item[0]}\nArea: {item[1]}\nPopulation: {item[2]}\n')
+        counter += 1
+
+
 if __name__ == '__main__':
     data_from_file = read_data_from_file('data.txt')
-    print(data_from_file)
+    print('Read data from file\n==========')
+    pretty_print_the_data(data_from_file)
     sorted_data_by_population = sort_by_population(data_from_file, True)
-    print(sorted_data_by_population)
+    print('Sorted data from file by population\n==========')
+    pretty_print_the_data(sorted_data_by_population)
     sorted_data_by_area = sort_by_area(data_from_file, True)
-    print(sorted_data_by_area)
+    print('Sorted data from file by area\n==========')
+    pretty_print_the_data(sorted_data_by_area)
 
