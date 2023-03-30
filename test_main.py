3cf596f613f12266a597eaecf72987b89276f135
Yevhen <eug.z2004@gmail.com>
2023-03-30 11:37:34 +0300

    added test_sort_by_area

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 import pytest
-from main import sort_by_population
+from main import sort_by_population, sort_by_area
 
 
 @pytest.mark.parametrize(
@@ -17,3 +17,20 @@ from main import sort_by_population
 )
 def test_sort_by_population(data, descending_order, result):
     assert sort_by_population(data, descending_order) == result
+
+
+@pytest.mark.parametrize(
+    'data, descending_order, result',
+    [
+        ([['Ukraine', 602000, 38000000], ['Poland', 312696, 37500000], ['USA', 9834000, 331900000], ['Germany', 357588, 83200000], ['Japan', 377973, 125700000]],
+        True,
+        [['USA', 9834000, 331900000], ['Ukraine', 602000, 38000000], ['Japan', 377973, 125700000], ['Germany', 357588, 83200000], ['Poland', 312696, 37500000]]
+         ),
+([['Ukraine', 602000, 38000000], ['Poland', 312696, 37500000], ['USA', 9834000, 331900000], ['Germany', 357588, 83200000], ['Japan', 377973, 125700000]],
+        False,
+        [['Poland', 312696, 37500000], ['Germany', 357588, 83200000], ['Japan', 377973, 125700000], ['Ukraine', 602000, 38000000], ['USA', 9834000, 331900000]]
+         )
+    ]
+)
+def test_sort_by_area(data, descending_order, result):
+    assert sort_by_area(data, descending_order) == result
